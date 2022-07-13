61e5b01be5708eca93ed168e06cb3e4d0fedbd31
jonatan hamel <jonatan.hamel@gmail.com>
2022-07-13 09:45:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO List</title>
+</head>
+<body>
+
+    <ul>
+
+        <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, deleniti. Minus amet nam consequuntur officiis quia porro, reiciendis magni ipsum minima soluta totam consequatur cum itaque iusto doloribus fugit eligendi!
+        </li>
+
+        <li>
+            Eos quo repellendus magnam rerum voluptatum magni aliquam quas dolorem, ducimus quasi numquam facilis laudantium consectetur animi. Officiis nam hic in nisi et eos accusamus rem consequuntur. Nulla, vitae animi.
+        </li>
+
+        <li>
+            Libero eveniet animi necessitatibus aspernatur ducimus temporibus aliquid laudantium error vitae neque delectus ea iure quod officiis, in culpa consectetur! Rerum corrupti consequuntur debitis incidunt perspiciatis molestiae cum, eius vero?
+        </li>
+
+        <li>
+            Consectetur nobis velit ratione aperiam pariatur, sequi culpa eligendi magni reiciendis ducimus eius facere? Dolor optio sequi, delectus, maxime tempora ducimus maiores nemo modi praesentium impedit nulla porro consectetur vitae.
+        </li>
+
+        <li>
+            Fuga eius amet omnis laudantium voluptatibus suscipit voluptatum quasi cum qui quidem. Minima sequi numquam quo recusandae molestiae facere illum saepe nemo nihil cupiditate iusto, accusantium, nobis ullam rerum hic?
+        </li>
+
+    </ul>
+    
+</body>
+</html>
